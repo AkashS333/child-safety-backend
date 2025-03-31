7b7b971adb37481076e2eeb4bc628f83600fcf22
AkashS333 <118100085+AkashS333@users.noreply.github.com>
2025-03-31 19:37:15 +0530

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render!"
+    return "Hello, Render Deployment!"
 
 if __name__ == '__main__':
-    app.run(debug=True)  # This line is ignored in production
+    app.run()
